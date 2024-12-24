a1163e90d287eca4008ffcd50477509a2c00bde4
John Grinwis <john@grinwis.com>
2024-12-24 15:07:46 +0100

    update output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
 output "dv_txt_records" {
   description = "Show DNS txt records that need to be created for validation"
-  value       = akamai_cps_dv_enrollment.certificate_enrollment
+  value       = akamai_cps_dv_enrollment.certificate_enrollment.dns_challenges
+}
+
+output "sans" {
+  description = "Show DNS txt records that need to be created for validation"
+  value       = akamai_cps_dv_enrollment.certificate_enrollment.sans
 }
